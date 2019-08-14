429d665dcb3000d0c5aabb37a160ad692889a537
keithcurtis1 <keithcurtis@gmail.com>
2019-08-14 00:24:43 -0700

    Update dealer.js

--- dealer.js
+++ dealer.js
@@ -7,7 +7,7 @@ on('chat:message', (msg) => {
         let action = args[1].split(/\s+/)[0];
         let numCards = args[1].split(/\s+/)[1];
         numCards = Number((Number.isInteger(Number(numCards))) ? numCards : 1);
-        log("numCards = "+numCards)
+        log("numCards = " + numCards)
 
 
         const actions = ['give', 'take'];
@@ -16,101 +16,101 @@ on('chat:message', (msg) => {
             cardAction = action;
         }
         let deckChoice = args[2] || 'Playing Cards';
-log('card action is '+cardAction);
+        log('card action is ' + cardAction);
         do {
 
-        //getid of deck
-        let theDeck = findObjs({
-            _type: "deck",
-            name: deckChoice
-        })[0];
+            //getid of deck
+            let theDeck = findObjs({
+                _type: "deck",
+                name: deckChoice
+            })[0];
 
-        //test if deck exists
-        if (!theDeck) {
-            sendChat('Deal', '/w gm Create a deck named ' + deckChoice + '. If the intent is an Inspiration deck, it must be an infinite deck of one card only.');
-            return;
-        }
-
-
-        let deckID = theDeck.id;
-        let deckCards = theDeck.get('_currentDeck');
-        log('The deck cards are ' + deckCards);
-        // Necessary to shuffle at least once after deck creation because of Roll20 Deck bug
-        //shuffleDeck(deckID);
-        //get id of card
-        let cardid = drawCard(deckID);
-        
-        if (!cardid){
-        shuffleDeck(deckID);
-cardid = drawCard(deckID);
-}
-        // get playerId of Token controller
-        //assign selected token to a variable
-
-        if (msg.selected.length > 1) {
-            sendChat('Deal', '/w gm Please select only one token. It must represent player-controlled character.');
-            return;
-        }
-
-        let token = getObj(msg.selected[0]._type, msg.selected[0]._id);
-
-        //assign associated character to a variable
-        if (!token.get('represents')) {
-            sendChat('Deal', '/w gm This token does not represent a player character. Only players get cards.');
-            return;
-        }
-        let character = getObj("character", token.get('represents'));
-
-        //Get owner IDs of each -- Not needed at this point
-        // If the token represents a character, get the character's controller, otherwise the token's
-        let ownerids = (token.get('controlledby').split(','));
+            //test if deck exists
+            if (!theDeck) {
+                sendChat('Deal', '/w gm Create a deck named ' + deckChoice + '. If the intent is an Inspiration deck, it must be an infinite deck of one card only.');
+                return;
+            }
 
 
-        if (character) {
-            ownerids = (character.get('controlledby').split(','));
-        }
-        //reduces to one ownerid that is not ['all']
-        ownerid = ownerids.filter(s => s !== 'all')[0];
+            let deckID = theDeck.id;
+            let deckCards = theDeck.get('_currentDeck');
+            log('The deck cards are ' + deckCards);
+            // Necessary to shuffle at least once after deck creation because of Roll20 Deck bug
+            //shuffleDeck(deckID);
+            //get id of card
+            let cardid = drawCard(deckID);
+
+            if (!cardid) {
+                shuffleDeck(deckID);
+                cardid = drawCard(deckID);
+            }
+            // get playerId of Token controller
+            //assign selected token to a variable
+
+            if (msg.selected.length > 1) {
+                sendChat('Deal', '/w gm Please select only one token. It must represent player-controlled character.');
+                return;
+            }
+
+            let token = getObj(msg.selected[0]._type, msg.selected[0]._id);
+
+            //assign associated character to a variable
+            if (!token.get('represents')) {
+                sendChat('Deal', '/w gm This token does not represent a player character. Only players get cards.');
+                return;
+            }
+            let character = getObj("character", token.get('represents'));
+
+            //Get owner IDs of each -- Not needed at this point
+            // If the token represents a character, get the character's controller, otherwise the token's
+            let ownerids = (token.get('controlledby').split(','));
 
 
-        // give card to player
-        // If the ownerid is undefined (no valid controller) perform the transaction
-        if (!ownerid) {
-            sendChat('deal', '/w gm If a token represents a character controlled by \'All Players\', an individual player must be also be specified. If there are multiple controllers, only the first will get inspiration.');
-            return;
-        }
+            if (character) {
+                ownerids = (character.get('controlledby').split(','));
+            }
+            //reduces to one ownerid that is not ['all']
+            ownerid = ownerids.filter(s => s !== 'all')[0];
 
-        switch (cardAction) {
-            case 'take':
 
-                let hand = findObjs({
-                    type: 'hand',
-                    parentid: ownerid
-                })[0];
-                let theHand = hand.get('currentHand');
+            // give card to player
+            // If the ownerid is undefined (no valid controller) perform the transaction
+            if (!ownerid) {
+                sendChat('deal', '/w gm If a token represents a character controlled by \'All Players\', an individual player must be also be specified. If there are multiple controllers, only the first will get inspiration.');
+                return;
+            }
 
-                cardid = (theHand.split(',').filter(x => deckCards.split(',').includes(x)))[0];
+            switch (cardAction) {
+                case 'take':
 
-                if (theHand.length !== 0 && cardid !== undefined) {
+                    let hand = findObjs({
+                        type: 'hand',
+                        parentid: ownerid
+                    })[0];
+                    let theHand = hand.get('currentHand');
 
-                    takeCardFromPlayer(ownerid, {
-                        cardid: cardid
-                    });
-                } else {
-                    let deckName = theDeck.get('name');
-                    sendChat('deal', '/w gm ' + token.get('name') + ' has no cards left to take from the ' + deckName + ' deck.');
-                }
+                    cardid = (theHand.split(',').filter(x => deckCards.split(',').includes(x)))[0];
 
-                break;
-            default:
+                    if (theHand.length !== 0 && cardid !== undefined) {
+
+                        takeCardFromPlayer(ownerid, {
+                            cardid: cardid
+                        });
+                    } else {
+                        let deckName = theDeck.get('name');
+                        sendChat('deal', '/w gm ' + token.get('name') + ' has no cards left to take from the ' + deckName + ' deck.');
+                    }
+
+                    break;
+                default:
                     giveCardToPlayer(cardid, ownerid);
                     break;
             }
 
-        
-        numCards--;
-        log("numCards = "+numCards)
-    }
-    while (numCards > 0);
+
+            numCards--;
+            log("numCards = " + numCards)
+        }
+        while (numCards > 0);
     }
 });
